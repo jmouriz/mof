e1a4aeed42181795ecd3311ff3063119af3537e9
Juan Manuel <juanma@localhost.localdomain>
2016-10-26 10:49:35 -0300

    Primera copia

---
+++ mof.php
@@ -0,0 +1,130 @@
+<?php 
+
+function password($password, $hash = null) {
+   if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
+      if ($hash) {
+         return password_verify($password, $hash);
+      }
+      return password_hash($password, PASSWORD_BCRYPT);
+   } else {
+      if ($hash) {
+         return $hash == crypt($password, $hash);
+      }
+      $prefix = '$2y$07$';
+      $blowfish = bin2hex(openssl_random_pseudo_bytes(22));
+      return crypt($password, "$prefix$blowfish");
+   }
+}
+
+function filename($backtrace) {
+   $path = __DIR__;
+   $backtrace = $backtrace[0];
+   $filename = $backtrace['file']; 
+   $line = $backtrace['line'] -1;
+   $file = file($filename);
+   $input = $file[$line];
+   preg_match('#\\$(\w+)#', $input, $match);
+   $name = $match[1];
+   return "$path/database/$name.dbz";
+}
+
+function store($variable) {
+   $backtrace = debug_backtrace();
+   $filename = filename($backtrace);
+   $data = serialize($variable);
+   $raw = gzdeflate($data, 1);
+   file_put_contents($filename, $raw);
+}
+
+function restore(&$variable) {
+   $backtrace = debug_backtrace();
+   $filename = filename($backtrace);
+   if (file_exists($filename)) {
+      $raw = file_get_contents($filename);
+      $data = gzinflate($raw);
+      $variable = unserialize($data);
+   } else {
+      $variable = array();
+   }
+}
+
+function input($variable) {
+   $post = filter_input(INPUT_POST, $variable);
+   $get = filter_input(INPUT_GET, $variable);
+   return $post ? $post : $get;
+}
+
+function session() {
+   $id = session_id();
+   if ($id == '') {
+      $session = session_start();
+      if ($session) {
+         return session_id();
+      } else {
+         return false;
+      }
+   } else {
+      return $id;
+   }
+}
+
+function protect() {
+   if (!logged()) {
+      json(array('status' => 'unauthorized'));
+      _assert('No autorizado');
+   }
+}
+
+function logged() {
+   session();
+   return isset($_SESSION['email']) ? $_SESSION['email'] : false;
+}
+
+function login($email) {
+   session();
+   $_SESSION['email'] = $email;
+   _log("Sesión iniciada como usuario $email");
+}
+
+function logout() {
+   session();
+   unset($_SESSION['email']);
+   session_destroy();
+}
+
+function _log($message, $dump = false) {
+   $path = __DIR__;
+   $file = "$path/logs/site.log";
+   $log = fopen($file, 'a') or die("¡No se pudo abrir el archivo $file!");
+   $now = date('d/m/Y H:i:s');
+   fwrite($log, $dump ? print_r($message, true) : "$now: $message\n");
+   fclose($log);
+}
+
+function debug($data) {
+   print '<pre>';
+   print_r($data);
+   print '</pre>';
+   exit(1);
+}
+
+function _assert($message, $file = __FILE__, $line = __LINE__) {
+   _log("assert => $message en $file ($line)");
+   exit(2);
+}
+
+function todo($task, $file, $line) {
+   _log("HACER '$task' en $file, ($line)");
+}
+
+function json($data) {
+   $json = json_encode($data);
+   $length = strlen($json);
+
+   header('Content-type: application/json; charset=utf-8');
+   header("Content-length: $length");
+
+   print $json;
+}
+
+?>
